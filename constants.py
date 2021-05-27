c4662146ecba7cb6cd612477b9032e5c7eceef2c
rustamwho <rustamwho@gmail.com>
2021-05-27 19:04:46 +0300

    All important constants in one file

---
+++ constants.py
@@ -0,0 +1,7 @@
+DIRECTORY_WITH_PHOTOS = 'C:/Dev/face_recognition_for_pacs/photos/'
+DIRECTORY_FOR_FACES = 'C:/Dev/face_recognition_for_pacs/face_images/'
+
+CAMS_OF_DOORS = {
+    'entry': 0,
+    'exit': 0,
+}
