9a897eb0f3c956b22ac862d1b2d8df2cd3e25ae8
rustamwho <rustamwho@gmail.com>
2021-05-31 22:07:49 +0300

    Class RecognitionProcess with use multiprocessing fro recognition

--- pacs.py
+++ pacs.py
@@ -3,103 +3,89 @@ import cv2
 import numpy as np
 import face_recognition
 import dlib
+from multiprocessing import Process, Queue
 
 from utils import (save_data_as_pickle, load_data_from_pickle,
                    load_images_and_encoding)
 
 dlib.DLIB_USE_CUDA = True
 
-known_faces_encodings = None
-known_persons = None
+
+class Person():
+    def __init__(self, name, image_path, access):
+        self.name = name
+        self.image_path = image_path
+        self.access = access
 
 
-def load_data_for_recognition():
-    global known_faces_encodings,known_persons
-    if os.path.exists('known_peoples.pickle'):
-        known_persons = load_data_from_pickle()
-    else:
-        known_persons = load_images_and_encoding()
-    known_faces_encodings = update_data()
+class RecognitionProcess(Process):
+    def __init__(self, name: str, image_queue: Queue, output_queue: Queue):
+        Process.__init__(self)
+        self.name = name
+        self.image_queue = image_queue
+        self.is_work = True
+        self.known_faces_encodings = None
+        self.known_persons = None
+        self.output_queue = output_queue
 
+        self.load_data_for_recognition()
 
-def update_data():
-    global known_persons
-    save_data_as_pickle(known_persons)
-    known_faces_encodings = []
-    for person in known_persons:
-        known_faces_encodings.append(person['face_encoding'])
-    return known_faces_encodings
+    def get_image(self):
+        if not self.image_queue.empty():
+            return True, self.image_queue.get()
+        else:
+            return False, None
 
+    def stop_process(self):
+        self.is_work = False
 
-def main_loop(known_persons=[]):
-    video_capture = cv2.VideoCapture(1, cv2.CAP_DSHOW)
-    known_faces_encodings = update_data(known_persons)
-    while True:
-        ret, frame = video_capture.read()
+    def run(self):
+        while self.is_work:
+            ret, image = self.get_image()
+            if ret:
+                self.recognition_employees(image)
 
-        # Quit when the input video file ends
-        if not ret:
-            break
+    def load_data_for_recognition(self):
+        if os.path.exists('known_peoples.pickle'):
+            self.known_persons = load_data_from_pickle()
+        else:
+            self.known_persons = load_images_and_encoding()
+        self.update_data()
 
-        rgb_frame = frame[:, :, ::-1]
+    def update_data(self):
+        save_data_as_pickle(self.known_persons)
+        self.known_faces_encodings = []
+        for person in self.known_persons:
+            self.known_faces_encodings.append(person['face_encoding'])
 
-        face_location = face_recognition.face_locations(rgb_frame)
+    def recognition_employees(self, image):
+        face_location = face_recognition.face_locations(image)
 
         if not face_location:
             print('None face')
-            continue
+            return None
 
-        face_encoding = face_recognition.face_encodings(rgb_frame,
+        face_encoding = face_recognition.face_encodings(image,
                                                         face_location)[0]
 
         face_distances = face_recognition.face_distance(
-            known_faces_encodings, face_encoding)
+            self.known_faces_encodings, face_encoding)
 
         best_match_index = np.argmin(face_distances)
 
         if face_distances[best_match_index] < 0.60:
-            name_guest = known_persons[best_match_index]['name']
-            access = known_persons[best_match_index]['access']
-            image_path = known_persons[best_match_index]['face_path']
+            name_guest = self.known_persons[best_match_index]['name']
+            access = self.known_persons[best_match_index]['access']
+            image_path = self.known_persons[best_match_index]['face_path']
             print(f'Guest: {name_guest}\n'
                   f'Access: {access}\n'
                   f'Image path: {image_path}')
+            self.output_queue.put({'door': self.name,
+                                   'name': name_guest,
+                                   'access': access,
+                                   'image_path': image_path})
         else:
             print('Unknown guest!')
-
-        # print(face_recognition.compare_faces(known_faces_encodings,face_encoding))
-        cv2.imshow('Video', frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
-
-
-def recognition_employees(image):
-    global known_faces_encodings, known_persons
-    face_location = face_recognition.face_locations(image)
-
-    if not face_location:
-        print('None face')
-        return None
-
-    face_encoding = face_recognition.face_encodings(image,
-                                                    face_location)[0]
-
-    face_distances = face_recognition.face_distance(
-        known_faces_encodings, face_encoding)
-
-    best_match_index = np.argmin(face_distances)
-
-    if face_distances[best_match_index] < 0.60:
-        name_guest = known_persons[best_match_index]['name']
-        access = known_persons[best_match_index]['access']
-        image_path = known_persons[best_match_index]['face_path']
-        print(f'Guest: {name_guest}\n'
-              f'Access: {access}\n'
-              f'Image path: {image_path}')
-    else:
-        print('Unknown guest!')
 
 
 if __name__ == "__main__":
