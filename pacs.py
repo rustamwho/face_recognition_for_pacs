a9b7c418d783c740ebf278c00b75a55f3a66de60
rustamwho <rustamwho@gmail.com>
2021-06-03 19:49:18 +0300

    - Delete unnecessary import - Moved get_connection, recognition and update bd to utils

--- pacs.py
+++ pacs.py
@@ -1,20 +1,14 @@
 import os
-import cv2
 import numpy as np
 import face_recognition
 import dlib
 from multiprocessing import Process, Queue
-import psycopg2
-from psycopg2 import Error
 
-from utils import (save_data_as_pickle, load_data_from_pickle,
-                   load_images_and_encoding, load_data_from_db)
-from settings import DATABASES_SERVER, DATABASE_NAME, THRESHOLD
-
-from required_classes import Person
+import utils
 
 dlib.DLIB_USE_CUDA = True
 
+# TODO: Добавить обновление базы
 
 class RecognitionProcess(Process):
     def __init__(self, name: str, image_queue: Queue, output_queue: Queue):
@@ -25,67 +19,35 @@ class RecognitionProcess(Process):
         self.is_work = True
 
         self.known_persons = self.load_data_for_recognition()
-        self.known_faces_encodings = self.update_data()
-
-    def get_connection(self):
-        connection_to_db = psycopg2.connect(host=DATABASES_SERVER['host'],
-                                            port=DATABASES_SERVER['port'],
-                                            user=DATABASES_SERVER['user'],
-                                            password=DATABASES_SERVER[
-                                                'password'],
-                                            database=DATABASE_NAME)
-        return connection_to_db
-
-    def recognise_employee_sql(self, encodings, connection):
-        """Распознавание сотрудника sql-запросом"""
-        cursor = connection.cursor()
-        try:
-            query = '''
-            SELECT first_name, last_name FROM employees 
-            WHERE sqrt( power( CUBE( array[{}]) <-> vec_low, 2) + 
-            power( CUBE( array[{}]) <-> vec_high, 2)) <= {} 
-            '''.format(
-                ','.join(str(s) for s in encodings[0:64]),
-                ','.join(str(s) for s in encodings[64:128]),
-                THRESHOLD,
-            ) + \
-                    '''
-                ORDER BY sqrt( power( CUBE( array[{}]) <-> vec_low, 2) + 
-                power( CUBE( array[{}]) <-> vec_high, 2)) ASC LIMIT 1'''.format(
-                ','.join(str(s) for s in encodings[0:64]),
-                ','.join(str(s) for s in encodings[64:128]),
-            )
-            cursor.execute(query)
-            print(cursor.fetchall())
-        except (Exception, Error) as error:
-            print('Ошибка при запросе к БД:', error)
+        self.known_faces_encodings = self.get_encodings_of_known_persons()
 
     def get_image(self):
+        """Получение изображения из очереди."""
         if not self.image_queue.empty():
             return True, self.image_queue.get()
         else:
             return False, None
 
     def stop_process(self):
+        """Остановка выполнения процесса."""
         self.is_work = False
 
     def run(self):
-        connection_to_db = self.get_connection()
+        connection_to_db = utils.get_connection_to_db()
         while self.is_work:
             ret, image = self.get_image()
             if ret:
                 self.recognition_employees(image, connection_to_db)
 
     def load_data_for_recognition(self):
+        # TODO: Исправить на проверку соединения с базой и наличием фоток
         if os.path.exists('known_peoples.pickle'):
             print('Загрузка из базы данных.')
-            return load_data_from_db()#load_data_from_pickle()
+            return utils.load_data_from_db()  # load_data_from_pickle()
         else:
-            return load_images_and_encoding()
+            return utils.load_images_and_encoding()
 
-    def update_data(self):
-        save_data_as_pickle(self.known_persons)
-
+    def get_encodings_of_known_persons(self):
         return [person.face_encoding for person in self.known_persons]
 
     def recognition_employees(self, image, connection_to_db):
@@ -98,7 +60,7 @@ class RecognitionProcess(Process):
         face_encoding = face_recognition.face_encodings(image,
                                                         face_location)[0]
 
-        self.recognise_employee_sql(face_encoding, connection_to_db)
+        utils.recognise_employee_sql(connection_to_db, face_encoding)
 
         face_distances = face_recognition.face_distance(
             self.known_faces_encodings, face_encoding)
