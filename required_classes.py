3865892ac88b943d4c30e22946820e1c33d2c0f9
rustamwho <rustamwho@gmail.com>
2021-06-03 19:43:55 +0300

    Add id to Person

--- required_classes.py
+++ required_classes.py
@@ -1,5 +1,6 @@
 class Person():
-    def __init__(self, name, image_path, access=True, face_encoding = None):
+    def __init__(self, name, image_path, id=0, access=True, face_encoding = None):
+        self.id = id
         self.name = name
         self.face_encoding = face_encoding
         self.image_path = image_path
